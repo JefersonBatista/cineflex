3bb1a81ec57dda245a42721975545e7de9ae6b6f
JefersonBatista <jeferson.de.oliveira.batista@gmail.com>
2021-12-14 15:36:49 -0300

    App header and container.

--- src/App/index.js
+++ src/App/index.js
@@ -5,23 +5,27 @@ import "./style.css";
 export default function App() {
   return (
     <div className="app">
-      <Button
-        size="large"
-        text="Testando o botão 1"
-        action={() => alert("testando componente botão 1")}
-      />
+      <header className="header">Cineflex</header>
 
-      <Button
-        size="large"
-        text="Testando o botão 2"
-        action={() => alert("testando componente botão 2")}
-      />
+      <div className="container">
+        <Button
+          size="large"
+          text="Testando o botão 1"
+          action={() => alert("testando componente botão 1")}
+        />
 
-      <Button
-        size="large"
-        text="Testando o botão 3"
-        action={() => alert("testando componente botão 3")}
-      />
+        <Button
+          size="large"
+          text="Testando o botão 2"
+          action={() => alert("testando componente botão 2")}
+        />
+
+        <Button
+          size="large"
+          text="Testando o botão 3"
+          action={() => alert("testando componente botão 3")}
+        />
+      </div>
     </div>
   );
 }
